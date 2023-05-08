602ff06350bf036862fe3bed18c2d8a8158ef649
Mathias Mogensencd collaction_website <mathiasrieckm@gmail.com>
2023-05-08 17:52:53 +0100

    feat: tic tac toe builder widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:tic_tac_toe_engine/engine/tic_tac_toe_controller.dart';
 import 'package:tic_tac_toe_engine/engine/tic_tac_toe_player.dart';
 import 'package:tic_tac_toe_engine/engine/tic_tac_toe_scoreboard.dart';
+import 'package:tic_tac_toe_engine/tic_tac_toe_engine.dart';
 
 void main() {
   runApp(const MaterialApp(
@@ -36,116 +36,114 @@ class _GameViewState extends State<GameView> {
         ],
       ),
     );
-
-    gameController.scoreboard.addListener(() {
-      if (mounted) {
-        setState(() {});
-      }
-    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(builder: (context, constraints) {
-      final isLarge = constraints.maxWidth > 420;
-      double minHeight = MediaQuery.of(context).size.height * 0.5;
-      if (minHeight < 300) minHeight = 300;
+    return TicTacToeBuilder(
+      controller: gameController,
+      builder: (context, state, scoreboard) => LayoutBuilder(
+        builder: (context, constraints) {
+          final isLarge = constraints.maxWidth > 420;
+          double minHeight = MediaQuery.of(context).size.height * 0.5;
+          if (minHeight < 300) minHeight = 300;
 
-      return Column(
-        children: [
-          const SizedBox(height: 50),
-          Expanded(
-            flex: 3,
-            child: SizedBox(
-              width: isLarge ? minHeight : 320,
-              height: isLarge ? minHeight : 320,
-              child: GridView.builder(
-                physics: const ClampingScrollPhysics(),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                ),
-                itemCount: 9,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(2.0),
-                    child: Container(
-                      color: Colors.orange.withOpacity(0.8),
+          return Column(
+            children: [
+              const SizedBox(height: 50),
+              Expanded(
+                flex: 3,
+                child: SizedBox(
+                  width: isLarge ? minHeight : 320,
+                  height: isLarge ? minHeight : 320,
+                  child: GridView.builder(
+                    physics: const ClampingScrollPhysics(),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
                     ),
-                  );
-                },
+                    itemCount: 9,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(2.0),
+                        child: Container(
+                          color: Colors.orange.withOpacity(0.8),
+                        ),
+                      );
+                    },
+                  ),
+                ),
               ),
-            ),
-          ),
-          Expanded(
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              Expanded(
+                child: Column(
                   children: [
-                    Text('Draws: ${gameController.scoreboard.draws}'),
-                    Text(
-                        'Player 1 wins: ${gameController.scoreboard.playerOne}'),
-                    Text(
-                        'Player 2 wins: ${gameController.scoreboard.playerTwo}'),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text('Draws: ${scoreboard.draws}'),
+                        Text('Player 1 wins: ${scoreboard.playerOne}'),
+                        Text('Player 2 wins: ${scoreboard.playerTwo}'),
+                      ],
+                    ),
+                    const SizedBox(height: 15),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            /// 1 2 2
+                            /// 2 1 1
+                            /// 1 1 2
+                            gameController.makeMove(0); // Player 1
+                            gameController.makeMove(1); // Player 2
+                            gameController.makeMove(4); // Player 1
+                            gameController.makeMove(2); // Player 2
+                            gameController.makeMove(5); // Player 1
+                            gameController.makeMove(3); // Player 2
+                            gameController.makeMove(6); // Player 1
+                            gameController.makeMove(8); // Player 2
+                            gameController.makeMove(7); // Player 1
+                          },
+                          child: const Text('Draw!'),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            /// 1 2 0
+                            /// 2 1 0
+                            /// 0 0 1
+                            gameController.makeMove(0); // Player 1
+                            gameController.makeMove(3); // Player 2
+                            gameController.makeMove(4); // Player 1
+                            gameController.makeMove(1); // Player 2
+                            gameController.makeMove(8); // Player 1
+                          },
+                          child: const Text('Player One!'),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            /// 1 1 0
+                            /// 2 2 2
+                            /// 1 0 0
+                            gameController.makeMove(0); // Player 1
+                            gameController.makeMove(3); // Player 2
+                            gameController.makeMove(1); // Player 1
+                            gameController.makeMove(4); // Player 2
+                            gameController.makeMove(6); // Player 1
+                            gameController.makeMove(5); // Player 2
+                          },
+                          child: const Text('Player Two!'),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 15),
+                    Text('Current Player: ${state.currentPlayer}'),
                   ],
                 ),
-                const SizedBox(height: 15),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    TextButton(
-                      onPressed: () {
-                        /// 1 2 2
-                        /// 2 1 1
-                        /// 1 1 2
-                        gameController.makeMove(0); // Player 1
-                        gameController.makeMove(1); // Player 2
-                        gameController.makeMove(4); // Player 1
-                        gameController.makeMove(2); // Player 2
-                        gameController.makeMove(5); // Player 1
-                        gameController.makeMove(3); // Player 2
-                        gameController.makeMove(6); // Player 1
-                        gameController.makeMove(8); // Player 2
-                        gameController.makeMove(7); // Player 1
-                      },
-                      child: const Text('Draw!'),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        /// 1 2 0
-                        /// 2 1 0
-                        /// 0 0 1
-                        gameController.makeMove(0); // Player 1
-                        gameController.makeMove(3); // Player 2
-                        gameController.makeMove(4); // Player 1
-                        gameController.makeMove(1); // Player 2
-                        gameController.makeMove(8); // Player 1
-                      },
-                      child: const Text('Player One!'),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        /// 1 1 0
-                        /// 2 2 2
-                        /// 1 0 0
-                        gameController.makeMove(0); // Player 1
-                        gameController.makeMove(3); // Player 2
-                        gameController.makeMove(1); // Player 1
-                        gameController.makeMove(4); // Player 2
-                        gameController.makeMove(6); // Player 1
-                        gameController.makeMove(5); // Player 2
-                      },
-                      child: const Text('Player Two!'),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 15),
-                Text('Current Player: ${gameController.state.currentPlayer}'),
-              ],
-            ),
-          ),
-        ],
-      );
-    });
+              ),
+            ],
+          );
+        },
+      ),
+    );
   }
 }
--- packages/tic_tac_toe_engine/lib/tic_tac_toe_engine.dart
+++ packages/tic_tac_toe_engine/lib/tic_tac_toe_engine.dart
@@ -2,3 +2,4 @@ library tic_tac_toe_engine;
 
 export './engine/tic_tac_toe_controller.dart';
 export './engine/tic_tac_toe_state.dart';
+export './flutter/tic_ta_toe_builder.dart';
